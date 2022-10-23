0c8547fad0b6dcc8919e91d244dbf81cabd8652b
Oleg Palashynskyi <palah@ukr.net>
2022-10-23 19:48:02 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,6 +17,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'contacts',
   storage,
+  whitelist: ['contacts'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
